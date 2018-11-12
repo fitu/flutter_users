816f49a11ba6c45f270720c9975df20d76d30005
Victorio Matteucci <victorio.matteucci@gmail.com>
2018-11-12 12:06:28 -0300

    feat: add request

--- lib/ui/pages/users_page.dart
+++ lib/ui/pages/users_page.dart
@@ -1,6 +1,30 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class UsersPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class UsersPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _UsersPageState();
+}
+
+class _UsersPageState extends State<UsersPage> {
+  static const String URL = 'https://randomuser.me/api/';
+
+  @override
+  void initState() {
+    doRequest();
+    super.initState();
+  }
+
+  Future<void> doRequest() async {
+    final response = await http.get(URL);
+    setState(() {
+      var body = json.decode(response.body);
+      print(body['results']);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
