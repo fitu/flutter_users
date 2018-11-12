b8e75831877609b7d0c5ab4ecb98806a5ba8bb85
Victorio Matteucci <victorio.matteucci@gmail.com>
2018-11-12 11:37:36 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_random_user/ui/pages/users_page.dart';
+
+void main() => runApp(UsersPage());
---
+++ lib/ui/pages/users_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UsersPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Random User',
+      home: Scaffold(
+        body: Center(),
+      ),
+    );
+  }
+}
